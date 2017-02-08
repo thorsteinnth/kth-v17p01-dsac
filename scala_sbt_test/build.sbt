1ab9a2c4b96dd79e9525dfe33c1db2968c98f686
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-02-08 23:26:01 +0100

    adding scala hello world test

---
+++ scala_sbt_test/build.sbt
@@ -0,0 +1,6 @@
+name := "scala_sbt_test"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ scala_sbt_test/src/main/scala/HelloWorld.scala
@@ -0,0 +1,7 @@
+/**
+  * Created by tts on 08/02/2017.
+  */
+object HelloWorld extends App
+{
+  println("hello world, scala works")
+}
