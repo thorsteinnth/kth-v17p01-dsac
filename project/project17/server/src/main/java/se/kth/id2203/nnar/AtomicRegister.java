6d4843b4c9b34dd95b7ccd0d7ec053d466e7dd19
Fannar Magnússon <fannar@kth.se>
2017-02-23 16:03:28 +0100

    atomic register start handler that initializes all fields

--- project/project17/server/src/main/java/se/kth/id2203/nnar/AtomicRegister.java
+++ project/project17/server/src/main/java/se/kth/id2203/nnar/AtomicRegister.java
@@ -6,6 +6,9 @@ import se.kth.id2203.broadcast.beb.BestEffortBroadcastPort;
 import se.kth.id2203.networking.NetAddress;
 import se.sics.kompics.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AtomicRegister extends ComponentDefinition {
 
     private final static Logger LOG = LoggerFactory.getLogger(AtomicRegister.class);
@@ -17,6 +20,15 @@ public class AtomicRegister extends ComponentDefinition {
     // Fields
     private final NetAddress self = config().getValue("id2203.project.address", NetAddress.class);
 
+    private Tuple tuple; //ts and wr
+    private Object value;
+    private int acks;
+    private Object readVal;
+    private Object writeVal;
+    private int rId;
+    private Map<NetAddress, Tuple> readList;
+    private boolean reading;
+
     // Handlers
     protected final Handler<Start> startHandler = new Handler<Start>() {
 
@@ -24,6 +36,14 @@ public class AtomicRegister extends ComponentDefinition {
         public void handle(Start event) {
 
             LOG.info("Starting Atomic Register on {}", self);
+            tuple = new Tuple(0, 0);
+            value = null;
+            acks = 0;
+            readVal = null;
+            writeVal = null;
+            rId = 0;
+            readList = new HashMap<>();
+            reading = false;
         }
     };
 
---
+++ project/project17/server/src/main/java/se/kth/id2203/nnar/Tuple.java
@@ -0,0 +1,82 @@
+package se.kth.id2203.nnar;
+
+public class Tuple {
+
+    private int ts;
+    private int wr;
+
+    private Object optionalValue;
+
+    public Tuple(int ts, int wr) {
+        this.ts = ts;
+        this.wr = wr;
+    }
+
+    public Tuple(int ts, int wr, Object optionalValue) {
+        this.ts = ts;
+        this.wr = wr;
+        this.optionalValue = optionalValue;
+    }
+
+    public int getTs() {
+        return ts;
+    }
+
+    public void setTs(int ts) {
+        this.ts = ts;
+    }
+
+    public int getWr() {
+        return wr;
+    }
+
+    public void setWr(int wr) {
+        this.wr = wr;
+    }
+
+    public Object getOptionalValue() {
+        return optionalValue;
+    }
+
+    public void setOptionalValue(Object optionalValue) {
+        this.optionalValue = optionalValue;
+    }
+
+    /**
+     * Checks if pair one is bigger then pair two
+     * @param tupleOne
+     * @param tupleTwo
+     * @return
+     */
+    public boolean biggerThan(Tuple tupleOne, Tuple tupleTwo) {
+
+        if (tupleOne.getTs() > tupleTwo.getTs()) {
+
+            if (tupleOne.getWr() > tupleTwo.getWr()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tuple tuple = (Tuple) o;
+
+        if (ts != tuple.ts) return false;
+        if (wr != tuple.wr) return false;
+        return optionalValue != null ? optionalValue.equals(tuple.optionalValue) : tuple.optionalValue == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = ts;
+        result = 31 * result + wr;
+        result = 31 * result + (optionalValue != null ? optionalValue.hashCode() : 0);
+        return result;
+    }
+}
